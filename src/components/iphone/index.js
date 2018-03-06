17da30d6b42cc15143d905a24744a4db04e2d532
Eden Ruffell <edenruffell@edens-air.home>
2018-03-06 16:41:05 +0000

    move 5 day forcast and add sunrise/sunset

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -16,6 +16,9 @@ import weatherkey from '../../weatherKey';
 
 import FutureWeather from '../future_weather';
 
+	
+
+
 export default class Iphone extends Component {
 //var Iphone = React.createClass({
 
@@ -52,6 +55,7 @@ export default class Iphone extends Component {
 		// // once the data grabbed, hide the button
 		this.setState({ display: false });
 	}
+
 	componentDidMount = () => {
 		this.fetchWeatherData();
 	}
@@ -66,13 +70,16 @@ export default class Iphone extends Component {
 				<div class={ style.header }>
 					<div class={ style.city }>{ weather.location }</div>
 				
-					<span>{ weather.temp_c }°C</span>
+					<div class={ style.temp}>{ weather.temp_c }°C</div>
 					<div class={ style.conditions }>Wind: { weather.windspeed } m/s</div>
 					<div class={ style.conditions }>{ weather.condition } </div>
-					<div class={ style.conditions }><img src={conditionsIconSrc} alt='Icon depicting current weather.'/></div>
+					<div class={ style.icon }><img src={conditionsIconSrc} alt='Icon depicting current weather.'/></div>
 					
 				</div>
-				<div class={ style.details }></div>
+			<div class="row">
+				<div class={style.suncolumn}>{weather.sunrise}</div>
+				<div class={style.suncolumn}>{weather.sunset}</div>
+			</div>
 				<div class= { style_iphone.container }>
 					
 				</div>
@@ -85,6 +92,7 @@ export default class Iphone extends Component {
 
 	parseResponse = (parsed_json) => {
 		// console.log(JSON.stringify(parsed_json));
+		
 		let location = parsed_json.name;
 		let temp_c = parsed_json.main.temp;
 		let weather = parsed_json.weather;
@@ -94,6 +102,14 @@ export default class Iphone extends Component {
 		let icon = weather[0].icon;
 		let windspeed = parsed_json.wind.speed;
 		let winddir = parsed_json.wind.deg;
+		var moment = require('moment');
+		
+		let sunrise = moment.unix(parsed_json.sys.sunrise).format('LT');
+		let sunset = moment.unix(parsed_json.sys.sunset).format('LT');
+
+	
+		console.log(parsed_json.sys.sunrise, parsed_json.sys.sunset );
+
 		// set states for fields so they could be rendered later on
 		this.setState({
 			weather: {
@@ -102,7 +118,9 @@ export default class Iphone extends Component {
 				condition,
 				windspeed,
 				description,
-				weatherid
+				weatherid,
+				sunrise, 
+				sunset
 			},
 			icon,
 			background: weatherkey.findBackgroundById(weatherid)
